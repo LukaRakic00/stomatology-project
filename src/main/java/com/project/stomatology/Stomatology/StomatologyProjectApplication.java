dd02c9d895b561bb8eba0784099fa8fb4008f2c9
Luka00 <rakicluka00@gmail.com>
2026-01-14 23:10:48 +0100

    Initial commit

---
+++ src/main/java/com/project/stomatology/Stomatology/StomatologyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.stomatology.Stomatology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StomatologyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StomatologyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/stomatology/Stomatology/StomatologyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stomatology.Stomatology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StomatologyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
